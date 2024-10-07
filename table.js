f94e5110a9fc38a58cefb15414c1e733a956022f
alecyalcin <alecyalcin@gmail.com>
2024-10-07 14:20:34 -0300

    receita8

--- table.js
+++ table.js
@@ -1,36 +1,42 @@
-const loadTable = (elements) => {
-  // Adicionar cabeçalho (atributos dos elementos)
-  const header = document.querySelector("#header");
-  let th_name = document.createElement("th");
-  th_name.innerHTML = "Nome";
-  header.appendChild(th_name);
-  let th_alcohol = document.createElement("th");
-  th_alcohol.innerHTML = "Alcool";
-  header.appendChild(th_alcohol);
-  let th_ibu = document.createElement("th");
-  th_ibu.innerHTML = "ibu";
-  header.appendChild(th_ibu);
-
-  //   Adicionar elementos do body
-  const body = document.querySelector("#body");
-
-  let td_name = document.createElement("td");
-  let td_alcohol = document.createElement("td");
-  let td_ibu = document.createElement("td");
-
-  const htmlItens = elements.map(({ name, alcohol, ibu }) => {
-    let tr_1 = document.createElement("tr");
-    tr_1.innerHTML = name;
-    td_name.appendChild(tr_1);
-    let tr_2 = document.createElement("tr");
-    tr_2.innerHTML = alcohol;
-    td_alcohol.appendChild(tr_2);
-    let tr_3 = document.createElement("tr");
-    tr_3.innerHTML = ibu;
-    td_ibu.appendChild(tr_3);
+const createTableHeader = (headers) => {
+  let headerRow = document.getElementById("table-head");
+  headers.forEach((headerText) => {
+    let th = document.createElement("th");
+    th.textContent = headerText;
+    headerRow.appendChild(th);
   });
-
-  body.appendChild(td_name);
-  body.appendChild(td_alcohol);
-  body.appendChild(td_ibu);
 };
+
+const populateTableBody = (elements) => {
+  const body = document.querySelector("#body");
+  elements.forEach(({ name, alcohol, ibu }) => {
+    const row = document.createElement("tr");
+
+    [name, alcohol, ibu].forEach((data) => {
+      let td = document.createElement("td");
+      td.textContent = data;
+      row.appendChild(td);
+    });
+
+    body.appendChild(row);
+  });
+};
+
+const loadTable = (elements) => {
+  createTableHeader(["Nome", "Álcool", "IBU"]);
+  populateTableBody(elements);
+};
+
+const carregarCervejas = () => {
+  const message = document.getElementById("message");
+  message.innerHTML = "Fazendo requisição...";
+
+  fetch("https://random-data-api.com/api/v2/beers?size=3")
+    .then((res) => res.json())
+    .then((res) => {
+      loadTable(res);
+      message.innerHTML = "";
+    });
+};
+
+document.getElementById("botaoCarregar").addEventListener("click", carregarCervejas);
