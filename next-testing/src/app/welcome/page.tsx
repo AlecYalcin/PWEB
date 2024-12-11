4880a948c21980d6519710a66a22a1c14968de9e
alecyalcin <alecyalcin@gmail.com>
2024-12-11 13:48:21 -0300

    Adicionado o next.js

---
+++ next-testing/src/app/welcome/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const welcome = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>I like potatoes.</p>
+    </div>
+  );
+};
+
+export default welcome;
